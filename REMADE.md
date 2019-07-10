ba79043c2ece698c4f56e54ed00041e27d78de0d
xxxx591 <250173319@qq.com>
2019-07-11 00:05:49 +0800

    添加组件

--- REMADE.md
+++ REMADE.md
@@ -1,6 +1,6 @@
 ###奶叔工坊
 
-1. 接口文档地址：
+1. 接口文档地址：https://www.showdoc.cc/405921453364964    密码：Sinmore2019
 2. 设计图文档：https://lanhuapp.com/url/dHIUM-DH2wE
 3. 后台地址：http://nsgf.yanyongwang.cn  admin 123456
 4. AppID(应用ID) wxfa6b0d6185e50d3d 
---
+++ components/navbar/header.js
@@ -0,0 +1,25 @@
+// components/navbar/header.js
+var app = getApp()
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    height: app.globalData.height,
+    title:"国奥越野"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+})
