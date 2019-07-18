9232cde0b9c25fd264f2e2705a1515904a747f1f
xxxx591 <250173319@qq.com>
2019-07-18 23:40:04 +0800

    购物车模块

--- pages/home/details/details.js
+++ pages/home/details/details.js
@@ -1,93 +1,78 @@
-// pages/home/list/list.js
-var app = getApp();
+// pages/home/details/details.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    leftList:[
-      { name:'本周特价',id:0},
-      { name:'精品花束',id:1},
-      { name:'单头玫瑰',id:2},
-      { name:'多头玫瑰',id:3},
-      { name:'百合',id:4},
-      { name:'所有商品',id:5},
-    ],
-    leftIndex:0,
-    shopList:[
-      {
-        imgUrl:'/assets/image/test02.png',
-        title:'送长辈·真的爱你',
-        price:'234',
-        count:0
-      },
-      {
-        imgUrl:'/assets/image/test02.png',
-        title:'送长辈·真的爱你233',
-        price:'123',
-        count:0
-      },
-      {
-        imgUrl:'/assets/image/test02.png',
-        title:'送长辈·真的爱你2123',
-        price:'112',
-        count:0
-      },
-      {
-        imgUrl:'/assets/image/test02.png',
-        title:'送长辈·真的爱你112',
-        price:'332',
-        count:0
-      },
-    ],
-    showShopBox:false,
-    shopcountIndex:0,
-    shopcountPrice:0
+    shopList:[],
+    showShopBox: false,
+    shopcountIndex: 0,
+    shopcountPrice: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let arr = wx.getStorageSync('shopList')
+    let obj = wx.getStorageSync('shopDetails')
+    let shopcountIndex = 0;
+    let shopcountPrice = 0;
+    arr.map(item=>{
+      shopcountIndex+=parseInt(item.count);
+      shopcountPrice += parseInt(item.count * item.price)
+    })
+    
+    this.setData({
+      shopList:arr,
+      shopcountIndex: shopcountIndex,
+      shopcountPrice: shopcountPrice
+    })
   },
   // 打开购物车
-  showBox(){
+  showBox() {
     this.setData({
       showShopBox: true
     })
   },
   // 点击空白关闭购物车
-  onClose(){
+  onClose() {
     this.setData({
       showShopBox: false
     })
   },
   // 物品减少
-  subtracttap(item){
+  subtracttap(item) {
     // console.log(item.currentTarget.dataset.count)
     let index = item.currentTarget.id
     let count = item.currentTarget.dataset.count
-    count ==0? 0:count--;
+    count == 0 ? 0 : count--;
     let deletedtodo = `shopList[${index}].count`;
     let allCount = this.data.shopcountIndex;
-    allCount==0?0:allCount--;
-    let allPrice= this.data.shopcountPrice;
+    allCount == 0 ? 0 : allCount--;
+    let allPrice = this.data.shopcountPrice;
     let price = parseInt(item.currentTarget.dataset.objs.price);
     allPrice = allPrice - price;
     this.setData({
-      [deletedtodo]:count,
+      [deletedtodo]: count,
       shopcountIndex: allCount,
       shopcountPrice: allPrice
     })
     wx.setStorageSync('shopDetails', this.data.shopList)
   },
   // 物品增加
-  addtap(item){
+  addtap(item) {
     // console.log(item.currentTarget.dataset.objs)
     let count = item.currentTarget.dataset.count
     let index = item.currentTarget.id
-    count ++;
+    count++;
     let deletedtodo = `shopList[${index}].count`;
     let allCount = 1;
     let allPrice = this.data.shopcountPrice;
     let price = parseInt(item.currentTarget.dataset.objs.price);
-    allPrice = allPrice+price;
-    (this.data.shopList).map(item=>{
+    allPrice = allPrice + price;
+    (this.data.shopList).map(item => {
       allCount += parseInt(item.count)
     })
     this.setData({
@@ -98,34 +83,16 @@ Page({
     wx.setStorageSync('shopDetails', this.data.shopList)
   },
   // 清空购物车
-  clearShopList(){
+  clearShopList() {
     let arr = this.data.shopList
-    arr.map(item=>{
+    arr.map(item => {
       item.count = 0
     })
     this.setData({
-      shopList:arr,
+      shopList: arr,
       showShopBox: false
     })
   },
-  changeIndex(e){
-    // console.log(e.currentTarget.id)
-    this.setData({
-      leftIndex: e.currentTarget.id
-    })
-  },
-  goDetails(e){
-    console.log(e.currentTarget.dataset.objs)
-    wx.setStorageSync('shopDetails', e.currentTarget.dataset.objs)
-    wx.setStorageSync('shopList', this.data.shopList)
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-     
-  },
-   
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -6,10 +6,6 @@ Page({
     logs: []
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
+    
   }
 })
